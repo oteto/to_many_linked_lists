05e82ec1ee6180edd95a938270f019cc11fbe33a
oteto <etochan5@gmail.com>
2023-01-13 17:03:19 +0900

    Generics 化

--- src/ok_stack.rs
+++ src/ok_stack.rs
@@ -1,20 +1,20 @@
-pub struct OkStack {
-    head: Link,
+pub struct OkStack<T> {
+    head: Link<T>,
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
-impl OkStack {
+impl<T> OkStack<T> {
     pub fn new() -> Self {
         OkStack { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem,
             next: self.head.take(),
@@ -22,7 +22,7 @@ impl OkStack {
         self.head = Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
@@ -30,7 +30,7 @@ impl OkStack {
     }
 }
 
-impl Drop for OkStack {
+impl<T> Drop for OkStack<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
         while let Some(mut boxed_node) = cur_link {
